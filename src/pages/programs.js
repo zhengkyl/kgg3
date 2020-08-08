5a8bf7e557f7a68a9fd55db861f719277b2d404d
Kyle Zheng <kylezheng73@gmail.com>
2020-08-07 20:02:24 -0400

    program pictures

--- src/pages/programs.js
+++ src/pages/programs.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react"
 import { graphql, Link } from "gatsby"
 import Img from "gatsby-image"
-
+import clsx from "clsx"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 import SEO from "../components/seo"
@@ -25,47 +25,37 @@ const useStyles = makeStyles(theme => ({
   },
   blockImg: {
     // maxWidth: 600,
-    minWidth: 240,
+    minWidth: 270,
   },
   programBlock: {
-    display: "flex",
-    flexWrap: "wrap",
-    backgroundColor: '#1f1f1f',
-    borderRadius:theme.spacing(2),
-    paddingLeft:theme.spacing(4),
-    paddingTop:theme.spacing(4),
-    overflow:'hidden',
-    // maxWidth: 600,
-    // backgroundColor: theme.palette.background.paper,
-    // flex:1,
-    // flexGrow:1,
+    backgroundColor: "#1f1f1f",
+    borderRadius: theme.spacing(2),
+    overflow: "hidden",
+    margin: theme.spacing(1),
+    // marginRight:theme.spacing(1),
   },
-  programsBlock:{
-    display:'flex',
-    flexWrap:'wrap',
-  }
+  programsBlock: {
+    display: "flex",
+    // flexWrap:'wrap',
+  },
+  programText: {
+    padding: theme.spacing(2),
+  },
 }))
 
 const ProgramsPage = props => {
   const classes = useStyles()
 
-  const Test = () => {
-    return (
-      <h1>hi</h1>
-    )
-  }
-  const ProgramBlock = ({title, desc, children}) => (
+  const ProgramBlock = ({ title, desc, children }) => (
     <>
       <div className={classes.programBlock}>
-        <div>
+        <div className={classes.programText}>
           <Typography variant="h5" className={classes.sectionTitle}>
             {title}
           </Typography>
           <Typography variant="body1">{desc}</Typography>
         </div>
         {children}
-        {/* <Img fluid={image} className={classes.blockImg} /> */}
-        {/* <div className={classes.colorBlock}></div> */}
       </div>
     </>
   )
@@ -80,40 +70,110 @@ const ProgramsPage = props => {
         <Typography variant="h4">
           KGG is proud to run the following events
         </Typography>
-        <div className={classes.programsBlock}>
-          <ProgramBlock title="Ramen Night" desc="Eat ramen at night">
-            <Img fluid={props.data.ramenNight.childImageSharp.fluid} className={classes.blockImg} />
-          </ProgramBlock>
-          <ProgramBlock title="Ramen Night" desc="Eat ramen at night">
-            <Img fluid={props.data.mentorship.childImageSharp.fluid} className={classes.blockImg} />
-          </ProgramBlock>
-          <ProgramBlock title="Ramen Night" desc="Eat ramen at night">
-            <Img fluid={props.data.advice.childImageSharp.fluid} className={classes.blockImg} />
-          </ProgramBlock>
-        </div>
-        
       </Container>
+      <div className={classes.programsBlock}>
+        <ProgramBlock title="Ramen Night" desc="Eat ramen at night">
+          <Img
+            fluid={props.data.ramenNight.childImageSharp.fluid}
+            className={classes.blockImg}
+          />
+        </ProgramBlock>
+        <ProgramBlock title="Ramen Night" desc="Eat ramen at night">
+          <Img
+            fluid={props.data.study.childImageSharp.fluid}
+            className={classes.blockImg}
+          />
+        </ProgramBlock>
+        <ProgramBlock title="Fashion Week" desc="Wear clothes all week">
+          <Img
+            fluid={props.data.slide.childImageSharp.fluid}
+            className={classes.blockImg}
+          />
+        </ProgramBlock>
+        {/* <ProgramBlock title="Youth Mentoring" desc="Help underprivileged youth">
+            <Img fluid={props.data.mentorship.childImageSharp.fluid} className={classes.blockImg}/>
+          </ProgramBlock> */}
+        <ProgramBlock title="Group Therapy" desc="Let's talk about stuff">
+          <Img
+            fluid={props.data.duo.childImageSharp.fluid}
+            className={classes.blockImg}
+          />
+        </ProgramBlock>
+        {/* <ProgramBlock title="Personal Trainer" desc='1 on 1 private "lessons"'>
+            <Img fluid={props.data.advice.childImageSharp.fluid} className={classes.blockImg}/>
+          </ProgramBlock> */}
+        <ProgramBlock title="Legal Counsel" desc="*not an attorney replacement">
+          <Img
+            fluid={props.data.legal.childImageSharp.fluid}
+            className={classes.blockImg}
+          />
+        </ProgramBlock>
+        <ProgramBlock
+          title="Math Tutoring"
+          desc="Acquire mathematical knowledge"
+        >
+          <Img
+            fluid={props.data.tutor.childImageSharp.fluid}
+            className={classes.blockImg}
+          />
+        </ProgramBlock>
+      </div>
     </>
   )
 }
 
 export const pageQuery = graphql`
   query {
-    ramenNight: file(relativePath: { eq: "ramen_night.png" }) {
+    ramenNight: file(relativePath: { eq: "ramen_night43.png" }) {
       childImageSharp {
         fluid(maxWidth: 300) {
           ...GatsbyImageSharpFluid_noBase64
         }
       }
     }
-    mentorship: file(relativePath: { eq: "mentorship.png" }) {
+    mentorship: file(relativePath: { eq: "mentorship43.png" }) {
       childImageSharp {
         fluid(maxWidth: 300) {
           ...GatsbyImageSharpFluid_noBase64
         }
       }
     }
-    advice: file(relativePath: { eq: "advice.png" }) {
+    advice: file(relativePath: { eq: "advice43.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 300) {
+          ...GatsbyImageSharpFluid_noBase64
+        }
+      }
+    }
+    legal: file(relativePath: { eq: "legal43.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 300) {
+          ...GatsbyImageSharpFluid_noBase64
+        }
+      }
+    }
+    tutor: file(relativePath: { eq: "tutor43.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 300) {
+          ...GatsbyImageSharpFluid_noBase64
+        }
+      }
+    }
+    duo: file(relativePath: { eq: "duo43.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 300) {
+          ...GatsbyImageSharpFluid_noBase64
+        }
+      }
+    }
+    slide: file(relativePath: { eq: "slide43.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 300) {
+          ...GatsbyImageSharpFluid_noBase64
+        }
+      }
+    }
+    study: file(relativePath: { eq: "study43.png" }) {
       childImageSharp {
         fluid(maxWidth: 300) {
           ...GatsbyImageSharpFluid_noBase64
